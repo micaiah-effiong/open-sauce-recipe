4e44378128eedc3bf5bf111f523458a48362c154
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-10 18:00:10 +0100

    add: creating important model fields and work structure

--- models/review.js
+++ models/review.js
@@ -1,5 +1,20 @@
 module.exports = function (sequelize, DataType) {
-  let model = sequelize.define("review", {});
+  let model = sequelize.define("review", {
+    words: {
+      type: DataType.STRING,
+      allowNull: true,
+    },
+    rating: {
+      type: DataType.INTEGER,
+      allowNull: false,
+    },
+  });
+
+  // instance methods
+  // model.prototype.methodName
+
+  // class methods
+  // model.methodName
 
   return model;
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,48 @@
 module.exports = function (sequelize, DataType) {
-  let model = sequelize.define("user", {});
+  let model = sequelize.define("user", {
+    firstname: {
+      type: DataType.STRING,
+      allowNull: false,
+      // validate length
+    },
+    lastname: {
+      type: DataType.STRING,
+      allowNull: false,
+      // validate length
+    },
+    country: {
+      type: DataType.STRING,
+      allowNull: false,
+      // validate length
+    },
+    email: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        // validate length
+      },
+    },
+    password: {
+      type: DataType.VIRTUAL,
+    },
+    salt: {
+      type: DataType.STRING,
+      allowNull: false,
+      // validate length
+    },
+    hash: {
+      type: DataType.STRING,
+      allowNull: false,
+      // validate length
+    },
 
+    // instance methods
+    // model.prototype.methodName
+
+    // class methods
+    // model.methodName
+  });
   return model;
 };
 
