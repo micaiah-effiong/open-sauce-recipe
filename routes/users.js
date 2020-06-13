3e9aabcfd106b9494c5c7e753453a56c13ada90e
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-13 09:36:58 +0100

    create: setup full endpoint for /users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a GET / resource");
+});
+
+router.get("/:id", function (req, res, next) {
+  res.send("respond with a GET /:id resource");
+});
+
+router.post("/", function (req, res, next) {
+  res.send("respond with a POST / resource");
+});
+
+router.put("/:id", function (req, res, next) {
+  res.send("respond with a PUT / resource");
+});
+
+router.delete("/:id", function (req, res, next) {
+  res.send("respond with a DELETE / resource");
 });
 
 module.exports = router;
