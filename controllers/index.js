77018f161c4a504fb90e39925c4e9d368632da13
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-14 13:01:41 +0100

    setting up controllers structure

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+let usersController = require("./users");
+let recipesController = require("./recipes");
+let variationsController = require("./variations");
+let reviewsController = require("./reviews");
+
+module.exports = (db) => {
+  return {
+    users: usersController(db),
+    recipes: recipesController(db),
+    variations: variationsController(db),
+    reviews: reviewsController(db),
+  };
+};
---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+module.exports = (db) => {
+  return {
+    getSingle: (req, res, next) => {},
+    getAll: (req, res, next) => {},
+    create: (req, res, next) => {},
+    update: (req, res, next) => {},
+    delete: (req, res, next) => {},
+  };
+};
