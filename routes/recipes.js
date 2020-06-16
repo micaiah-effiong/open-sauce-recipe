ac0ac04413c3a2af4b7dc59c165660107fc98206
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-16 16:19:48 +0100

    add async handler for recipe route controllers

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,11 +6,16 @@ let {
   update,
   deleteSingle,
 } = require("../controllers/index").recipes;
+let asyncHandler = require("../handlers/async-handler");
 var router = express.Router();
 
 /* GET recipes listing. */
-router.route("/").get(getAll).post(create);
+router.route("/").get(asyncHandler(getAll)).post(asyncHandler(create));
 
-router.route("/:id").get(getSingle).put(update).delete(deleteSingle);
+router
+  .route("/:id")
+  .get(asyncHandler(getSingle))
+  .put(asyncHandler(update))
+  .delete(asyncHandler(deleteSingle));
 
 module.exports = router;
