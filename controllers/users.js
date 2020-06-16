029abcdf8b8f11b9f57fe925fffc9508a30f3d93
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-16 13:47:04 +0100

    improving on CRUD setup

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,72 @@
 const asyncHandler = require("../handlers/async-handler");
+const _ = require("underscore");
 
 module.exports = (db) => {
   return {
     getSingle: asyncHandler(async (req, res, next) => {
-      let user = await db.user.findByPk(req.params.id);
+      const { id } = req.params;
+      id = Number(id);
+      let user = await db.user.findByPk(id);
       res.json({
         success: true,
-        data: user.toJSON(),
+        data: user.toPublicJSON(),
       });
     }),
     getAll: asyncHandler(async (req, res, next) => {
-      let user = await db.user.findAll();
+      let users = await db.user.findAll();
+      let data = users.map((user) => user.toPublicJSON());
       res.json({
         success: true,
-        data: user,
+        data,
+        count: data.length,
       });
     }),
     create: asyncHandler(async (req, res, next) => {
-      let user = await db.user.create(req.body);
+      const {
+        body: { firstname, lastname, country, email, password },
+      } = req;
+
+      let user = await db.user.create({
+        firstname,
+        lastname,
+        country,
+        email,
+        password,
+      });
       res.json({
         success: true,
         data: user.toPublicJSON(),
       });
     }),
     update: asyncHandler(async (req, res, next) => {
-      let user = await db.user.update({
+      let { body } = req;
+      let cleanBody = _.omit(
+        body,
+        "salt",
+        "hash",
+        "createdAt",
+        "updatedAt",
+        "password",
+        "role"
+      );
+      const { id } = req.params;
+      id = Number(id);
+      let data = await db.user.update(cleanBody, {
         where: {
           id: req.params.id,
         },
       });
       res.json({
         success: true,
-        data: user.toJSON(),
+        data,
       });
     }),
     deleteSingle: asyncHandler(async (req, res, next) => {
-      let user = await db.user.destroy(req.params.id);
+      const { id } = req.params;
+      id = Number(id);
+      let data = await db.user.destroy({ where: { id } });
       res.json({
         success: true,
-        data: user.toJSON(),
       });
     }),
   };
