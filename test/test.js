a7a82ee7245b052d967a24cc22a312a8de0c2c58
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-18 12:50:34 +0100

    added testing for POST /users. configured sequelize to use in-memory storage for testing

--- test/test.js
+++ test/test.js
@@ -1,15 +1,18 @@
+process.env.NODE_ENV = "mocha_test";
 const { expect } = require("chai");
 const app = require("../app");
 const db = require("../models/index");
-const post = require("request");
-const get = require("request");
+const { post } = require("request");
+const { get } = require("request");
 let server;
 
 before((done) => {
   db.sequelize
     .sync({ force: false })
     .then(() => {
-      server = app.listen(3000, done);
+      console.log(db.sequelize.options);
+      console.log(process.env.NODE_ENV);
+      server = app.listen(3001, done);
     })
     .catch((err) => {
       console.log(err);
@@ -17,15 +20,44 @@ before((done) => {
 });
 
 describe("test", () => {
-  const body = {};
-  before((done) => {
-    get("http://localhost:3000/test", (errorr, res) => {
-      body.response = res.body;
-      done();
+  describe("get", () => {
+    const body = {};
+    before((done) => {
+      get("http://localhost:3001/test", (error, res) => {
+        body.response = res.body;
+        done();
+      });
+    });
+    it("should return a string", () => {
+      expect(body.response).to.equal("ci with travis");
     });
   });
-  it("should return a string", () => {
-    expect(body.response).to.equal("ci with travis");
+  describe("post", () => {
+    const body = {};
+    before((done) => {
+      post(
+        "http://localhost:3001/users",
+        {
+          json: {
+            firstname: "Micaiah",
+            lastname: "Effiong",
+            country: "NG",
+            gender: "M",
+            email: "micaiah.effiong@gmail.com",
+            password: "nnnnnnnn",
+            role: "admin",
+          },
+        },
+        (error, res) => {
+          body.response = res.body;
+          done();
+        }
+      );
+    });
+    it("should return an object", () => {
+      console.log(typeof body.response, body.response);
+      expect(body.response.success).to.equal(true);
+    });
   });
 });
 
