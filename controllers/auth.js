b0b3ae3ff16dd23ace0fcf6676b357bb43bd1bd1
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-20 19:22:57 +0100

    fix crash commit issues: move user registration to auth controller, set up login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,13 +3,27 @@ const errorResponse = require("../handlers/error-response");
 
 module.exports = (db) => {
   return {
-    logout: asyncHandler((req, res, next) => {
-      res.clearCookie("auth").redirect("/");
-    }),
-    login: asyncHandler((req, res, next) => {
-      console.log(req.user);
+    register: asyncHandler(async (req, res, next) => {
+      let user = await db.user.create(req.body);
       res.json({
         success: true,
+        data: user.toPublicJSON(),
+      });
+    }),
+    logout: asyncHandler((req, res, next) => {
+      req.logout();
+      res.redirect("/");
+    }),
+    login: asyncHandler((req, res, next) => {
+      let { user } = req;
+      req.login(user, function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.json({
+          success: true,
+          data: user.toPublicJSON(),
+        });
       });
     }),
   };
