f49438ec0ce229233d60d9b6929386fa2d71d338
micaiah-effiong <micaiah.effiong@gmail.com>
2020-06-22 10:21:17 +0100

    fix crash commit issues: Sanitize user input. Refactor: create variation with user and recipe associations

--- controllers/variations.js
+++ controllers/variations.js
@@ -1,16 +1,17 @@
 const asyncHandler = require("../handlers/async-handler");
+const errorResponse = require("../handlers/error-response");
 
 module.exports = (db) => {
   return {
     getSingle: asyncHandler(async (req, res, next) => {
-      let { id } = req.params;
-      id = Number(id);
+      let id = Number(req.params.id);
       let variation = await db.variation.findByPk(id);
       res.json({
         success: true,
         data: variation.toJSON(),
       });
     }),
+
     getAll: asyncHandler(async (req, res, next) => {
       let variations = await db.variation.findAll();
       let data = variations.map((variation) => variation.toJSON());
@@ -20,16 +21,23 @@ module.exports = (db) => {
         data,
       });
     }),
+
     create: asyncHandler(async (req, res, next) => {
-      let variation = await db.variation.create(req.body);
+      let { rid: id } = req.params;
+      let recipe = await db.recipe.findByPk(id);
+      if (!recipe) {
+        return next(errorResponse("Bad Request 2", 400));
+      }
+      let userVariation = await req.user.createVariation(req.body);
+      let variation = await recipe.addVariation(userVariation);
       res.json({
         success: true,
-        data: variation,
+        data: userVariation,
       });
     }),
+
     update: asyncHandler(async (req, res, next) => {
-      let { id } = req.params;
-      id = Number(id);
+      let id = Number(req.params.id);
       await db.variation.update(req.body, {
         where: { id },
       });
@@ -37,9 +45,9 @@ module.exports = (db) => {
         success: true,
       });
     }),
+
     deleteSingle: asyncHandler(async (req, res, next) => {
-      let { id } = req.params;
-      id = Number(id);
+      let id = Number(req.params.id);
       await db.variation.destroy({
         where: { id },
       });
