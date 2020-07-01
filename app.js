77128273242aecd9534d18a67c49222e37189c05
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-01 16:29:39 +0100

    setup: sturctures for requesting and reseting passwords

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: "secret",
+    secret: process.env.SESSION_TOKEN,
     resave: false,
     saveUninitialized: false,
   })
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,19 @@ const errorResponse = require("../handlers/error-response");
 
 module.exports = (db) => {
   return {
-    register: asyncHandler(async (req, res, next) => {
+    register: asyncHandler(async function (req, res, next) {
       let user = await db.user.create(req.body);
       res.json({
         success: true,
         data: user.toPublicJSON(),
       });
     }),
+
     logout: asyncHandler((req, res, next) => {
       req.logout();
       res.redirect("/");
     }),
+
     login: asyncHandler((req, res, next) => {
       let { user } = req;
       req.login(user, function (err) {
@@ -25,6 +27,35 @@ module.exports = (db) => {
           data: user.toPublicJSON(),
         });
       });
+    }),
+
+    forgotPassword: asyncHandler(async function (req, res, next) {
+      /*
+        user makes a request with POST request
+        with e-mail in request body
+        genetrate password reset token (to expire in 20hr)
+        send token to user e-mail
+      */
+      let user = await db.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      let passwordResetToken = await user.getResetPasswordToken();
+    }),
+    resetPasswordEmailConfirmation: asyncHandler((req, res, next) => {
+      /*
+        GET
+        reset token is in users req query
+        verify and decode the token,
+        if decoded email matches user email
+      */
+    }),
+    resetPassword: asyncHandler((req, res, next) => {
+      /*
+        find user and update user password entry
+      */
     }),
   };
 };
