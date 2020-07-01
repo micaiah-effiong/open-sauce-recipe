e4615450660f43f8ed4af518c4658197c5f611d2
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-01 17:38:10 +0100

    add: request for a password reset and password reset confirmationfunctionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("../handlers/async-handler");
 const errorResponse = require("../handlers/error-response");
 
@@ -36,21 +37,39 @@ module.exports = (db) => {
         genetrate password reset token (to expire in 20hr)
         send token to user e-mail
       */
-      let user = await db.findOne({
+      let user = await db.user.findOne({
         where: {
           email: req.body.email,
         },
       });
 
       let passwordResetToken = await user.getResetPasswordToken();
+      res.json({
+        success: true,
+        token: passwordResetToken,
+      });
     }),
-    resetPasswordEmailConfirmation: asyncHandler((req, res, next) => {
+    resetPasswordEmailConfirmation: asyncHandler(async function (
+      req,
+      res,
+      next
+    ) {
       /*
         GET
         reset token is in users req query
         verify and decode the token,
         if decoded email matches user email
       */
+      let decoded = await jwt.verify(
+        req.query.key,
+        process.env.FORGOTTEN_PASSWORD_SECRET
+      );
+
+      console.log(decoded);
+      res.json({
+        success: true,
+        data: decoded,
+      });
     }),
     resetPassword: asyncHandler((req, res, next) => {
       /*
