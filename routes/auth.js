e6f93007a528c41b293f0da859cffb1084cfb6a8
micaiah-effiong <micaiah.effiong@gmail.com>
2020-07-01 17:44:14 +0100

    add routes and controllers for "/reset-password" and "/forgot-password" endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,20 @@
 var express = require("express");
-let { login, logout, register } = require("../controllers/index").auth;
+let {
+  login,
+  logout,
+  register,
+  forgotPassword,
+  resetPasswordEmailConfirmation,
+  resetPassword,
+} = require("../controllers/index").auth;
 var router = express.Router();
 let passport = require("../config/passport-config");
 
 router.route("/logout").get(logout);
+router.route("/reset-password").get(resetPasswordEmailConfirmation);
+
 router.route("/login").post(passport.authenticate("local"), login);
 router.route("/register").post(register);
-
+router.route("/forgot-password").post(forgotPassword);
+// /reset-password
 module.exports = router;
