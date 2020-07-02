4d473877ea239f37ecd235c4492b3a8ec090d2a3
Micaiah Effiong <52747707+micaiah-effiong@users.noreply.github.com>
2020-07-02 17:11:23 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
-process.env.NODE_ENV = "mocha_test";
 const { expect } = require("chai");
-const app = require("../app");
-const db = require("../models/index");
 const { post } = require("request");
 const { get } = require("request");
 const bcrypt = require("bcrypt");
+process.env.NODE_ENV = "mocha_test";
 process.env.SESSION_TOKEN = bcrypt.genSaltSync(10);
 process.env.FORGOTTEN_PASSWORD_SECRET = bcrypt.genSaltSync(10);
+const app = require("../app");
+const db = require("../models/index");
+
 let server;
 
 function parseBody(body) {
